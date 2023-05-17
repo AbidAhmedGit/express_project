f7aed772a82fbd6aa315850af6f7a66aa2c5964e
abid <abid@michaels.com>
2023-05-17 14:43:59 -0500

    sending pic.png as resopnse instead of json

--- controller/root.controller.js
+++ controller/root.controller.js
@@ -1,6 +1,10 @@
+const path = require('path');
 function getRoot(req, res) {
     // res.send('Hello from the server side!')
-    res.send('hello from the server side!')
+    // res.send('Hello from the server side!')
+
+    
+    res.sendFile(path.join(__dirname, '..', 'public', 'pic.png'));
 }
 
 module.exports = getRoot;
